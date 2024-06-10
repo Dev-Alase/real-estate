cdf156b0cc3a6ac5b3ea90f794165dc1797a4ab2
Dev-Alase <devalase5@gmail.com>
2024-06-10 12:43:20 +0530

    created and connected to database.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("Connected to Database")
+})
+.catch((err) => {
+    console.log(err)
+})
+
 
 const app = express();
 
 app.listen(4000,()=>{
     console.log("Server is running on port 4000")
 })
+
