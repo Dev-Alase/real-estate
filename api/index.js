bb31e3de9a43acf4da376c07d6a1e5e49957ec71
Dev-Alase <devalase5@gmail.com>
2024-06-11 12:51:34 +0530

    Added sign up page fucntionality { including UI (signUp.jsx),route(auth.route.js & auth.controller.js) ,handling form data }

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 
 const app = express();
+app.use(express.json())
 
 app.listen(4000,()=>{
     console.log("Server is running on port 4000")
@@ -22,3 +24,14 @@ app.listen(4000,()=>{
 
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+})
