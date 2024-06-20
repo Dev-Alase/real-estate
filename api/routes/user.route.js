cb210d9e8bfdf3dcd2fdfd6974a6a3c4253146d0
Dev-Alase <devalase5@gmail.com>
2024-06-20 11:50:20 +0530

    added api route for contact landlord

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
-import {updateUser,deleteUser,getUserListings} from "../controllers/user.controller.js"
+import {updateUser,deleteUser,getUserListings,getUser} from "../controllers/user.controller.js"
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router()
@@ -9,5 +9,6 @@ userRouter.get('/test',test)
 userRouter.post('/update/:id',verifyToken,updateUser)
 userRouter.delete('/delete/:id',verifyToken,deleteUser)
 userRouter.get('/listings/:id',verifyToken,getUserListings)
+userRouter.get('/:id',verifyToken,getUser)
 
 export default userRouter;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -267,11 +267,11 @@ export default function Profile() {
         {userListings.map((listing) => (
           <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
             
-            <Link to={`/listings/${listing._id}`}>
+            <Link to={`/listing/${listing._id}`}>
               <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain"/>         
             </Link>
 
-            <Link to={`/listings/${listing._id}`} className="flex-1">
+            <Link to={`/listing/${listing._id}`} className="flex-1">
               <p className="text-slate-700 font-semibold flex-1 hover:underline truncate">{listing.name}</p>       
             </Link>
 
