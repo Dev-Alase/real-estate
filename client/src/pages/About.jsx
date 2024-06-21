b3a43821289f2c675322510ed41d7ec8858be0b0
Dev-Alase <devalase5@gmail.com>
2024-06-21 15:41:20 +0530

    completed about page.and completed our project.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'> About Sahand Estate </h1>
+
+      <p className='mb-4 text-slate-700'>
+        Nothing.....
+      </p>
+    </div>
   )
 }
